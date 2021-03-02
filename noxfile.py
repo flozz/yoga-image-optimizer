54915b19c3ad2a07bc4196577e6321527e3fed18
Fabien LOISON <flo@flogisoft.com>
2021-03-02 18:23:05 +0100

    Only run lint on the CI as there is some more config to do to make tests pass

--- noxfile.py
+++ noxfile.py
@@ -7,7 +7,12 @@ def lint(session):
     session.run("flake8", "yoga_image_optimizer", "noxfile.py")
 
 
-@nox.session(python=["3.6", "3.7", "3.8", "3.9"])
+# NOTE All Gtk dependencies and introspection files must be installed for this
+# to work. You will also need the python3-gi package for the right Python3
+# version.
+@nox.session(
+        python=["3.6", "3.7", "3.8", "3.9"],
+        venv_params=["--system-site-packages"])
 def test(session):
     session.install("pytest")
     session.install(".")
