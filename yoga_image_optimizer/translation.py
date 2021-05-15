62b25b0663ea0b8acc6c687bb1d36259d6edd2be
Fabien LOISON <flo@flogisoft.com>
2021-05-15 15:16:11 +0200

    Remove useless local manipulation

--- yoga_image_optimizer/translation.py
+++ yoga_image_optimizer/translation.py
@@ -5,8 +5,6 @@ from . import APPLICATION_ID
 from . import data_helpers
 
 
-locale.bindtextdomain(APPLICATION_ID, data_helpers.find_data_path("locales"))
-
 translation = gettext.translation(
     APPLICATION_ID,
     localedir=data_helpers.find_data_path("locales"),
